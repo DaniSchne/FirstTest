b507c7445087293242e80decaacb796a51ca962e
D049251 <D049251@WDFN30055032A.emea.global.corp.sap>
2017-04-24 16:25:28 +0200

    Hello World

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-// Call the console.log function.
-console.log("Hello World");
+// 'Hallo Welt' Webserver
+var http = require('http');
+var port = 8080;
 
-console.log("Second test");
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hallo Welt!');
+});
+server.listen(port);
 
-console.log("Third test");
+console.log('Der Server ist erreichbar unter http://127.0.0.1:' + port + '/');
